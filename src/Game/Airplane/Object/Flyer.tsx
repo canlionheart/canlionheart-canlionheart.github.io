45afd4cef881f89614550f4c901683e01aa97897
ZhuBaba <gaudc@prtonmail.com>
2023-04-12 21:46:05 -0500

    now takes time to fly to target Y

--- src/Game/Airplane/Object/Flyer.tsx
+++ src/Game/Airplane/Object/Flyer.tsx
@@ -9,7 +9,7 @@ interface FlyerProps {
 export default function Flyer({ worldHeight }: FlyerProps) {
   const speed = 16;
   const flyerRef = useRef<HTMLDivElement>(null);
-  const [currentY, setCurrentY] = useState(0);
+  const [targetY, setTargetY] = useState(0);
 
   //
   const updateFlyerPosition = function(mouseY: number) {
@@ -20,15 +20,16 @@ export default function Flyer({ worldHeight }: FlyerProps) {
       const flyerHeight = bottom - top;
   
       const targetY = mouseY - flyerHeight;
-      console.log('world height: ' + worldHeight);
-        
-      console.log('flyer height: ' + flyerHeight);
-      console.log('mouse y: ' + mouseY);
-      console.log('targetY: ' + targetY);
+
+      // console.log('world height: ' + worldHeight);
+      // console.log('flyer height: ' + flyerHeight);
+      // console.log('mouse y: ' + mouseY);
+      // console.log('targetY: ' + targetY);
       const newY = Math.min(0, Math.max(targetY, flyerHeight*1.375) - worldHeight);
       //console.log('newY: ' + newY);
   
-      flyerRef.current!.style.transform = `translateY(${newY}px)`;
+      setTargetY(newY);
+      //flyerRef.current!.style.transform = `translateY(${newY}px)`;
     }
   }
 
@@ -43,16 +44,14 @@ export default function Flyer({ worldHeight }: FlyerProps) {
     };
   }, [updateFlyerPosition, worldHeight]);
 
-  /*
+  
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
 
     const moveFlyer = () => {
-      const flyerHeight = flyerRef.current?.getBoundingClientRect().height || 0;
-      const newY = Math.min(0, -1 * worldHeight + Math.max(currentY, flyerHeight * 1.375));
       const currentTransform = flyerRef.current?.style.transform;
       const currentYPos = currentTransform ? parseFloat(currentTransform.split(',')[1]) : 0;
-      const delta = (newY - currentYPos) * speed / 1000; // in pixels
+      const delta = (targetY - currentYPos) * speed / 1000; // in pixels
       const nextY = currentYPos + delta;
       flyerRef.current!.style.transform = `translate(0, ${nextY}px)`;
     };
@@ -62,8 +61,8 @@ export default function Flyer({ worldHeight }: FlyerProps) {
     return () => {
       clearInterval(intervalId);
     };
-  }, [currentY, speed, worldHeight]);
-*/
+  }, [targetY, speed, worldHeight]);
+
 
   const style = {};
 
